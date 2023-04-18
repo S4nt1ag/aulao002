4783a67a5a51d2753b3e337de76d40331ad48d30
Santiago <gabrielsantiagoneves2@gmail.com>
2023-04-17 21:02:32 -0300

    att

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,7 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 import entities.Product;
 
@@ -20,6 +13,29 @@ public class Program {
 
 		List<Product> list = new ArrayList<Product>();
 
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			String line = br.readLine();
+			line = br.readLine();
+			while (line != null) {
+
+				String[] vect = line.split(",");
+				String name = vect[0];
+				Double price = Double.parseDouble(vect[1]);
+				Integer qte = Integer.parseInt(vect[2]);
+
+				Product prod = new Product(name, price, qte);
+				list.add(prod);
+
+				line = br.readLine();
+			}
+			System.out.println("PRODUCTS:");
+			for (Product p : list) {
+				System.out.println(p);
+			}
+		} catch (IOException e) {
+			System.out.println("Error: linha invalida" + e.getMessage());
+		}
 		try {
 			FileWriter cadastro = new FileWriter(path, true);
 			BufferedWriter escrever = new BufferedWriter(cadastro);
