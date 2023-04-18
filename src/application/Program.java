0447ad93932e79f7930086b085ee6415c4e2931b
Santiago <gabrielsantiagoneves2@gmail.com>
2023-04-17 23:23:24 -0300

    atualização

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,12 +7,31 @@ import entities.Product;
 
 public class Program {
 
+	public static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
 
 		String path = "c:\\aulao002\\in.txt";
 
 		List<Product> list = new ArrayList<Product>();
 
+		// le o arquivo
+		ler(path, list);
+
+		// Listar produtos
+		System.out.println("PRODUCTS:");
+		for (Product p : list) {
+			System.out.println(p);
+		}
+
+		encontrarEatualizar("Teclado gamer", list, path);
+
+		criarNovoProduto(path, list);
+
+		ler(path, list);
+	}
+
+	public static void ler(String path, List<Product> list) {
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
 			String line = br.readLine();
@@ -29,17 +48,16 @@ public class Program {
 
 				line = br.readLine();
 			}
-			System.out.println("PRODUCTS:");
-			for (Product p : list) {
-				System.out.println(p);
-			}
+
 		} catch (IOException e) {
 			System.out.println("Error: linha invalida" + e.getMessage());
 		}
+	}
+
+	public static void criarNovoProduto(String path, List<Product> list) {
 		try {
 			FileWriter cadastro = new FileWriter(path, true);
 			BufferedWriter escrever = new BufferedWriter(cadastro);
-			Scanner sc = new Scanner(System.in);
 			System.out.println("Nome do produto: ");
 			String name = sc.nextLine();
 			System.out.println("Preço do produto: ");
@@ -55,31 +73,27 @@ public class Program {
 		} catch (InputMismatchException | IOException e1) {
 			e1.printStackTrace();
 		}
+	}
 
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+	public static void encontrarEatualizar(String nomeProcurado, List<Product> list, String path) {
+		for (Product product : list) {
+			if (product.getName().equalsIgnoreCase(nomeProcurado)) {
+				System.out.println("Encontrei");
+				System.out.println("Digite o novo nome: ");
+				String novoNome = sc.next();
+				product.setName(novoNome);
 
-			String line = br.readLine();
-			line = br.readLine();
-			while (line != null) {
+				try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+					for (Product product1 : list) {
+						bw.write(product1.getName() + "," + product1.getPrice() + "," + product1.getQuantity());
+						bw.newLine();
+					}
 
-				String[] vect = line.split(",");
-				String name = vect[0];
-				Double price = Double.parseDouble(vect[1]);
-				Integer qte = Integer.parseInt(vect[2]);
-
-				Product prod = new Product(name, price, qte);
-				list.add(prod);
-
-				line = br.readLine();
+				} catch (IOException e) {
+					System.out.println("Error:" + e.getMessage());
+				}
 			}
-			System.out.println("PRODUCTS:");
-			for (Product p : list) {
-				System.out.println(p);
-			}
-		} catch (IOException e) {
-			System.out.println("Error: linha invalida" + e.getMessage());
 		}
-
 	}
 
 }
